f91415787203adb1c7408e0fc0c54b3d5830d229
Harshdev321 <107427966+Harshdev321@users.noreply.github.com>
2023-02-19 14:04:27 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import './App.css';
+import React from 'react';
+import {add,sub,mult,div} from './Calc';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+
+
+function App() {   
+    return(
+    
+    <>
+    <ul>
+       <li>{add(40,3)}</li>
+       <li>{sub(40,3)}</li>
+       <li>{mult(4,3)}</li>
+       <li>{div(4,3)}</li>
+    
+    </ul>
+  
+  </>
+       
   );
 }
 
+
 export default App;
+
+
